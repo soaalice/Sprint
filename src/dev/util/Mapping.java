19a436ebf9b3cc6409a51db8f4e108b455759045
soaalice <168509452+soaalice@users.noreply.github.com>
2024-07-02 08:38:15 +0300

    Sprint 7

--- src/dev/util/Mapping.java
+++ src/dev/util/Mapping.java
@@ -2,7 +2,9 @@ package dev.util;
 
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
+import java.util.Enumeration;
 import java.util.HashMap;
+import java.util.Set;
 
 import mg.annotation.Param;
 
@@ -19,9 +21,68 @@ public class Mapping {
                 Param paramName=parameterFunction[i].getAnnotation(Param.class);
                 String parameterNameFunction=paramName.name();
                 //Atao cles ilay nom de parametres de raha tsy misy izy de null no azo
-                parameterValues[i]=requestParameter.get(parameterNameFunction);
+                System.out.println(parameterNameFunction);
+                if(parameterFunction[i].getType().isPrimitive() || parameterFunction[i].getType().equals(String.class)){
+                    parameterValues[i]=requestParameter.get(parameterNameFunction);
+                } 
+                else {
+                    HashMap<String,String> values=getAttributeValue(requestParameter,parameterNameFunction);
+                    Object objValue=parameterFunction[i].getType().getConstructor().newInstance();
+                    setValue(objValue, values);
+                    parameterValues[i]=objValue;
+                }
+                System.out.println(parameterValues[i]);
+            } 
+            // else if (parameterFunction[i] instanceof CustomSession) {
+                
+            // }
+            else {
+                throw new Exception("Ce parametre n'est pas annoté.");
             }
         }
         return this.methode.invoke(obj, parameterValues);
+    }
+
+    public HashMap<String,String> getAttributeValue(HashMap<String,String> requestParameter,String parameterName){
+        HashMap<String,String> valiny=new HashMap<String,String>();
+        Set<String> keys=requestParameter.keySet();
+        for (String key : keys) {
+            if(key.contains(parameterName+".")){
+                valiny.put(key.split("[.]")[1], requestParameter.get(key));
+            }
+        }
+        return valiny;
+    }
+
+    public void setValue(Object obj,HashMap<String,String> values)throws Exception{
+        Set<String> keys=values.keySet();
+        for (String key : keys) {
+            String setterName="set"+key.substring(0,1).toUpperCase()+key.substring(1);
+            Method[] methods=obj.getClass().getMethods();
+            Method setter=null;
+            System.out.println(methods.length);
+            for(int i=0;i<methods.length;i++){
+                System.out.println(setterName+" "+ methods[i].getName());
+                if(methods[i].getName().compareTo(setterName)==0 && methods[i].getParameterTypes().length==1){
+                    setter=methods[i];
+                    break;
+                }
+            }
+            Object value=parse(values.get(key),setter.getParameterTypes()[0]);
+            setter.invoke(obj, value);
+        }
+    }
+
+    public Object parse(String value,Class<?> type){
+        if(type == int.class){
+            return Integer.parseInt(value);
+        }
+        if(type == double.class){
+            return Double.parseDouble(value);
+        }
+        if (type == float.class) {
+            return Float.parseFloat(value);
+        }
+        return value;
     }
 }
