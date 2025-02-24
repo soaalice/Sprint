c4cc431da4d39ede4dbe3275f14ed1ed2d27a281
soaalice <amboararampanana@gmail.com>
2025-02-24 09:20:07 +0300

    Update RequiredValidator.java

--- src/mg/annotation/validations/validateur/RequiredValidator.java
+++ src/mg/annotation/validations/validateur/RequiredValidator.java
@@ -11,6 +11,11 @@ public class RequiredValidator extends Validator<Required>{
     @Override
     public Exception validate(Object o, Required annotation){
         if(o==null){
+            if (o instanceof String string) {
+                if (string.isEmpty()) {
+                    return new Exception(annotation.message());
+                }
+            }
             return new Exception(annotation.message());
         }
         return null;
